60ede966cafc12078fd1ad97df88e50b3ed14e88
Jack Herrington <jherr@pobox.com>
2020-04-27 06:26:24 -0700

    Adding date support

--- pages/api/days.js
+++ pages/api/days.js
@@ -1,5 +1,11 @@
 export default (req, res) => {
+  const today = new Date();
+  const lastDay = process.env.LAST_DAY
+    ? Date.parse(process.env.LAST_DAY)
+    : new Date();
+  const days = Math.floor((today - lastDay) / (24 * 60 * 60 * 1000));
+
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ days: 5 }));
+  res.end(JSON.stringify({ days }));
 };
